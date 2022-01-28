22506a48c516b9cca914a2ca8488fabcd8975d66
sungeuni0208 <sungeuni0208@naver.com>
2022-01-28 15:52:09 +0900

    Feat: [2- 클러스터 기능 추가]

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=>{
   res.render('index', { title: 'Express' });
+});
+//화살표 함수
+router.get('/upload', (req, res, next) => {
+  res.render('upload');
 });
 
 module.exports = router;
