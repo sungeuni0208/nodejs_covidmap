0511804b6c73d752c24a499adb49ea86a40dce16
sungeuni0208 <sungeuni0208@naver.com>
2022-03-16 01:57:35 +0900

    Feat: [2- 키워드 검색기능 추가]

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,18 @@ router.get('/upload', (req, res, next) => {
   res.render('upload');
 });
 
+router.get('/test', (req, res, next) => {
+  console.log("테스트 완료");
+  res.json({
+    message: "response 완료!!",
+  });
+});
+router.post('/test2', (req, res, next) => {
+  const { test, test2 } = req.body;
+  console.log(test,test2);
+  res.json({
+    message: "post 완료!",
+  });
+});
+
 module.exports = router;
